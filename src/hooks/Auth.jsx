d5005677f94d58f9f2b61cc1b391a44a02880173
Jack Hansjee <jack.hansjee@me.com>
2024-11-20 14:52:25 +0000

    Create a context and hook for accessing the authenticated user

---
+++ src/hooks/Auth.jsx
@@ -0,0 +1,55 @@
+import { createContext, useEffect } from "react";
+import supabase from "../../supabaseClient";
+
+const AuthContext = createContext({
+  session: null,
+  user: null,
+  signOut: () => {},
+});
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [session, setSession] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const setData = async () => {
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession();
+      if (error) throw error;
+      setSession(session);
+      setUser(session?.user);
+      setLoading(false);
+    };
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+        setUser(session?.user);
+        setLoading(false);
+      }
+    );
+
+    setData();
+
+    return () => {
+      listener?.subscription.unsubscribe();
+    };
+  }, []);
+
+  const value = {
+    session,
+    user,
+    signOut: () => supabase.auth.signOut(),
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
