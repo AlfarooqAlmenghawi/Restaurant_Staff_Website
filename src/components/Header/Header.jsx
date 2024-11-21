7c667071a4d58216c14c0567291fe56d99c36db4
Jack Hansjee <jack.hansjee@me.com>
2024-11-21 16:57:59 +0000

    Add import for styling sheets and correct issue with auth context

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,34 @@
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../../hooks/Auth";
+import "./Header.css";
 function Header() {
+  const { session, signOut } = useAuth();
   return (
     <>
-      <nav>
-        <h1 className="restaurant-title">Black Beards Grill</h1>
-        <Link className="heading-button" to="/test">
-          Test
-        </Link>
-        <h2>Place holder company name</h2>
-        <Link className="heading-button" to="/tables">
+      <h1 className="restaurant-title">Black Beards Grill</h1>
+      <NavLink className="heading-button" to="/test">
+        Test
+      </NavLink>
+      <nav className="nav-bar">
+        <h2 className="nav-item">Place holder company name</h2>
+        <NavLink className="nav-item" to="/tables">
           Tables
-        </Link>
-        <Link className="heading-button" to="/test2">
+        </NavLink>
+        <NavLink className="nav-item" to="/test2">
           Manage Profile
-        </Link>
-        <Link className="heading-button" to="/test2">
+        </NavLink>
+        <NavLink className="nav-item" to="/test2">
           Go Live
-        </Link>
+        </NavLink>
+        {session ? (
+          <button onClick={signOut} className="nav-item">
+            Sign Out
+          </button>
+        ) : (
+          <NavLink to="/sign-in" className="nav-item">
+            Sign In
+          </NavLink>
+        )}
       </nav>
     </>
   );
