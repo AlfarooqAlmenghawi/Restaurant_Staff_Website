63226f4835a998f3b5df93ddc112f36d4df0306d
Jack Hansjee <jack.hansjee@me.com>
2024-11-26 10:58:14 +0000

    Add logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,37 +3,30 @@ import { useAuth } from "../../hooks/Auth";
 import "./Header.css";
 import { useEffect, useState } from "react";
 import supabase from "../../../supabaseClient";
+import Logo from "../../svgs/logo.svg?react";
 
 function Header() {
   const { user, session, signOut } = useAuth();
-  const RestaurantID = Number(session?.restaurant_id);
-  const [currentRestaurantID, setCurrentRestaurantID] = useState(null);
-
-  const [currentRestaurant, setCurrentRestaurant] = useState("");
-
   useEffect(() => {
-    setCurrentRestaurantID(Number(session?.restaurant_id));
-    supabase
-      .from("restaurants")
-      .select("*, restaurant_cuisines(*)")
-      .eq("restaurant_id", RestaurantID)
-      .then(({ data, error }) => {
-        console.log(data || error);
-        setCurrentRestaurant(data[0].restaurant_name);
-      });
+    session?.restaurant_id &&
+      supabase
+        .from("restaurants")
+        .select("*, restaurant_cuisines(*)")
+        .eq("restaurant_id", session?.restaurant_id)
+        .then(({ data, error }) => {
+          console.log(data || error);
+          setCurrentRestaurant(data[0].restaurant_name);
+        });
   }, []);
 
   return (
     <>
-      {currentRestaurant ? (
+      <Logo />
+      {session?.restaurant_id && (
         <h1 className="restaurant-title">{currentRestaurant}</h1>
-      ) : (
-        <h1 className="restaurant-title">No Restaurant Selected</h1>
       )}
 
-      <nav className="nav-bar">
-        <h2 className="nav-item">Place holder company name</h2>
-
+      <nav className="flex">
         <NavLink className="@apply custButton" to="/my-restaurants">
           My Restaurants
         </NavLink>
