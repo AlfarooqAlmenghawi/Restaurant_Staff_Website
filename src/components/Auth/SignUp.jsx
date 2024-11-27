085bf4911c9d18b17f17527fab968098930ff501
Jack Hansjee <jack.hansjee@me.com>
2024-11-27 15:02:25 +0000

    Update sign-up page with loading state

--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -1,18 +1,52 @@
-import { Form, Link, redirect, useActionData } from "react-router-dom";
+import { Form, Link, redirect, useNavigate } from "react-router-dom";
 import supabase from "../../../supabaseClient";
 import { ErrMsg } from "./ErrMsg";
+import { useState } from "react";
+import { BarLoader } from "react-spinners";
 
 export const SignUp = () => {
-  const returnedData = useActionData();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const email = e.currentTarget[0].value;
+    const password = e.currentTarget[1].value;
+    const confirmPassword = e.currentTarget[2].value;
+
+    if (password !== confirmPassword) {
+      setLoading(false);
+      setError({ code: "Passwords don't match" });
+      return;
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: "https://localhost:5173.com/sign-in",
+      },
+    });
+    if (error) {
+      setLoading(false);
+      setError(error);
+      return;
+    }
+
+    navigate("/sign-in");
+  };
   return (
     <div className="pt-[20vh] flex justify-center items-center">
-      <Form method="post" action="/sign-up" className="authForm">
+      <Form method="post" onSubmit={handleSubmit} className="authForm">
         <h2 className="text-2xl">Staff Sign Up</h2>
         <div className="flex flex-col gap-4 w-full">
-          {returnedData?.error.code.startsWith("Passwords") && (
+          {error?.code.startsWith("Passwords") && (
             <ErrMsg>The passwords you entered don't match</ErrMsg>
           )}
-          {returnedData?.error.code === "email_exists" && (
+          {error?.code === "email_exists" && (
             <ErrMsg>
               The email you provided is already in use. Try{" "}
               <Link to="/sign-in">logging in</Link> instead
@@ -43,30 +77,14 @@ export const SignUp = () => {
             required
           />
         </div>
-        <button type="submit" className="authSubmit">
-          Sign Up
+        <button type="submit" className="authSubmit" disabled={loading}>
+          {loading ? (
+            <BarLoader color="white" className="my-3 mx-auto" />
+          ) : (
+            "Sign Up"
+          )}
         </button>
       </Form>
     </div>
   );
-};
-
-export const signUpAction = async ({ request }) => {
-  const submittedData = await request.formData();
-  if (submittedData.get("password") !== submittedData.get("confirmPassword"))
-    return { error: { code: "Passwords don't match" } };
-  const submission = {
-    email: submittedData.get("email"),
-    password: submittedData.get("password"),
-  };
-
-  const { data, error } = await supabase.auth.signUp({
-    ...submission,
-    options: {
-      emailRedirectTo: "https://localhost:5173.com/sign-in",
-    },
-  });
-  if (error) return { error };
-
-  return redirect("/my-restaurants");
 };
